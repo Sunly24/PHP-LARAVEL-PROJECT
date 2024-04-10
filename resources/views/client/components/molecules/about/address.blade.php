173dcdffda602316770d2fcdc24d73f846efb5a4
No Man <129272504+Sunly24@users.noreply.github.com>
2024-04-10 14:48:31 +0700

    Add files via upload

---
+++ resources/views/client/components/molecules/about/address.blade.php
@@ -0,0 +1,14 @@
+<div class="address py-md-5 py-2" id="address">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <h3 class="font-primary"><u>Address</u></h3>
+      </div>
+      <div class="col-lg-8 col-md-8 col-12">
+        <div class="responsive-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9446.915407716002!2d104.92832047706818!3d11.570666261375637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31095144cbdbf311%3A0x2588e1ac1787eb64!2sWat%20Phnom%20Daun%20Penh!5e0!3m2!1sen!2skh!4v1712486100856!5m2!1sen!2skh" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
