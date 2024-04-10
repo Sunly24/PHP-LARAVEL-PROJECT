cd57dea07e9d33d48b111d290832106e7a90bde4
No Man <129272504+Sunly24@users.noreply.github.com>
2024-04-10 14:46:18 +0700

    Delete resources/views directory

--- resources/views/client/carts.blade.php
+++
@@ -1,9 +0,0 @@
-<x-template.layout title="{{$title}}">
-    <x-organisms.navbar cartCount=10 :path="$shop->path"/>
-        <div class="container mt-3">
-            <h2>Carts</h2>
-            <hr/>
-        </div>
-    <x-organisms.carts />
-    <x-organisms.footer :shop="$shop"/>
-</x-template.layout>
--- resources/views/client/category.blade.php
+++
@@ -1,7 +0,0 @@
-<x-template.layout title="{{$title}}">
-  <x-organisms.navbar cartCount=10 :path="$shop->path"/>
-  <x-organisms.category :dataCategory="$category">
-    {{ $category->links('vendor.pagination.bootstrap-5') }}
-  </x-organisms.category>
-  <x-organisms.footer :shop="$shop"/>
-</x-template.layout>
